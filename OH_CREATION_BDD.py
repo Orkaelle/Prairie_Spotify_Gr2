570a2d021e82670877a566d0a5f5d2d03e578db0
Orkaëlle <orkaelle.hardelin@gmail.com>
2020-07-02 14:03:42 +0200

    Ajout data features

--- OH_CREATION_BDD.py
+++ OH_CREATION_BDD.py
@@ -16,7 +16,7 @@ cur = bdd.cursor()
 
 ## CREATION TABLES
 cur.execute('CREATE TABLE artiste (id_artiste INTEGER PRIMARY KEY AUTOINCREMENT, nom_artiste TEXT UNIQUE);')
-cur.execute('CREATE TABLE titre (id_titre INTEGER PRIMARY KEY AUTOINCREMENT, nom_titre TEXT UNIQUE, durée REAL, bpm REAL, energie REAL, intensité REAL, id_spotify text);')
+cur.execute('CREATE TABLE titre (id_titre INTEGER PRIMARY KEY AUTOINCREMENT, nom_titre TEXT UNIQUE, durée REAL, bpm REAL, energie REAL, intensité REAL, danceability REAL, speechiness REAL, liveness REAL, valence REAL, id_spotify text);')
 cur.execute('CREATE TABLE playlist (id_playlist INTEGER PRIMARY KEY AUTOINCREMENT, nom_playlist TEXT);')
 cur.execute('CREATE TABLE artiste_titre (titre_id INTEGER, artiste_id INTEGER, FOREIGN KEY (titre_id) REFERENCES titre(id_titre), FOREIGN KEY (artiste_id) REFERENCES artiste(id_artiste), PRIMARY KEY (titre_id, artiste_id));')
 cur.execute('CREATE TABLE playlist_titre (playlist_id INTEGER, titre_id INTEGER, FOREIGN KEY (playlist_id) REFERENCES paylist(id_playlist), FOREIGN KEY (titre_id) REFERENCES titre(id_titre), PRIMARY KEY (playlist_id, titre_id));')
