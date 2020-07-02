38281b6af9d9d58f15b547501768ed75c275c562
Orkaëlle <orkaelle.hardelin@gmail.com>
2020-07-02 13:23:03 +0200

    Noms uniques dans tables

--- OH_CREATION_BDD.py
+++ OH_CREATION_BDD.py
@@ -13,25 +13,20 @@ os.remove(path + "/bddSpotify.db")
 bdd = sqlite3.connect(path + "/bddSpotify.db")
 cur = bdd.cursor()
 
+
 ## CREATION TABLES
-cur.execute('CREATE TABLE artiste (id_artiste INTEGER PRIMARY KEY AUTOINCREMENT, nom_artiste TEXT);')
-cur.execute('CREATE TABLE titre (id_titre INTEGER PRIMARY KEY AUTOINCREMENT, nom_titre TEXT, durée REAL, bpm REAL, energie REAL, intensité REAL, id_spotify text);')
+cur.execute('CREATE TABLE artiste (id_artiste INTEGER PRIMARY KEY AUTOINCREMENT, nom_artiste TEXT UNIQUE);')
+cur.execute('CREATE TABLE titre (id_titre INTEGER PRIMARY KEY AUTOINCREMENT, nom_titre TEXT UNIQUE, durée REAL, bpm REAL, energie REAL, intensité REAL, id_spotify text);')
 cur.execute('CREATE TABLE playlist (id_playlist INTEGER PRIMARY KEY AUTOINCREMENT, nom_playlist TEXT);')
 cur.execute('CREATE TABLE artiste_titre (titre_id INTEGER, artiste_id INTEGER, FOREIGN KEY (titre_id) REFERENCES titre(id_titre), FOREIGN KEY (artiste_id) REFERENCES artiste(id_artiste), PRIMARY KEY (titre_id, artiste_id));')
 cur.execute('CREATE TABLE playlist_titre (playlist_id INTEGER, titre_id INTEGER, FOREIGN KEY (playlist_id) REFERENCES paylist(id_playlist), FOREIGN KEY (titre_id) REFERENCES titre(id_titre), PRIMARY KEY (playlist_id, titre_id));')
 print("Création des tables effectuée.")
 
-# ## AJOUT DE DONNEES
-# cur.execute('INSERT INTO artiste VALUES (1,"Claude Francois"),(2,"Madonna"),(3,"Lorie");')
-# cur.execute('INSERT INTO titre VALUES (1,"Cette année là",3.49,202,3.54,5.65),(2,"Like a virgin",3.32,120,6.33,8.76),(3,"Frozen",4.88,72,5.44,5.65);')
-# cur.execute('INSERT INTO artiste_titre VALUES (1,1),(2,2),(3,2);')
-# cur.execute('INSERT INTO playlist VALUES (1,"Planete Rap 1964"),(2,"Chansons Paillardes d''ici et d''ailleurs"),(3,"Love Night"),(4,"L''intégrale de JUL en Fa Mineur");')
-# cur.execute('INSERT INTO playlist_titre VALUES (2,2),(3,1),(3,2),(4,1),(4,2),(4,3);')
-# print("Ajout des datas OK.")
 
 ## SAUVEGARDE DE LA BDD
 bdd.commit()
 print("BDD sauvegardée.")
+
 
 ## FERMETURE BDD
 bdd.close()
